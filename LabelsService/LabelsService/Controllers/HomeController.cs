dc04afd23ccde9838836de2b42363b4241408840
Yan499732 <61782789+Yan499732@users.noreply.github.com>
2021-05-11 14:16:02 +0700

    Commit-6
    Добавлены обработчики ошибок. Также добавлены сообщения об удачной/неудачной попытке сгенерировать документ. Реализована работа с БД

--- LabelsService/LabelsService/Controllers/HomeController.cs
+++ LabelsService/LabelsService/Controllers/HomeController.cs
@@ -28,8 +28,9 @@ namespace LabelsService.Controllers
         }
 
         [HttpPost]
-        public string Index(string URL)
+        public IActionResult Index(string URL)
         {
+            DateTime start = DateTime.Now;
             string label = Request.Form["val1[]"];
             string value = Request.Form["val2[]"];
             string[] type = Request.Form["undefined[]"];
@@ -37,103 +38,177 @@ namespace LabelsService.Controllers
             Dictionary<string, string> TextLabels = new Dictionary<string, string>();
             Dictionary<string, string> PictureLabels = new Dictionary<string, string>();
             Dictionary<string, string[]> TableLabels = new Dictionary<string, string[]>();
+            Image[] image = new Image[100];
+            int index = 0;
+            TextRange range = null;
+            TextSelection[] selection = new TextSelection[100];
             string path = new KnownFolder(KnownFolderType.Downloads).Path;
-
+            string errormessage = "Создать документ не удалось из-за следующих ошибок:\n";
+            bool error = false;
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;User Id=postgres;Password=123;Database=LabelsServiceDB;");
+            string patholdword = path + @"\Документ(Шаблон).docx";
+            string pathnewword = path + @"\Документ(Результат).docx";
+            string imagespaths = "";
+            //Удаление лишних пропусков
             label = label.Replace("⁣", "");
             value = value.Replace("⁣", "");
-
+            //Разделение переменной на массив
             string[] labels = label.Split(',');
             string[] values = value.Split(',');
 
-            for (int i = 0; i < type.Length; i++)
+            try
             {
-                //Текст
-                if (type[i] == "Текстовая метка") TextLabels.Add(labels[i], values[i]);
-                //Изображения
-                if (type[i] == "Метка изображения") PictureLabels.Add(labels[i], values[i]);
-                //Таблицы
-                if (type[i] == "Метка таблицы") TableLabels.Add(labels[i], Regex.Split(values[i], "\\, "));
+                for (int i = 0; i < type.Length; i++)
+                {
+                    //Текст
+                    if (type[i] == "Текстовая метка") TextLabels.Add(labels[i], values[i]);
+                    //Изображения
+                    if (type[i] == "Метка изображения") PictureLabels.Add(labels[i], values[i]);
+                    //Таблицы
+                    if (type[i] == "Метка таблицы") TableLabels.Add(labels[i], Regex.Split(values[i], "\\, "));
+                }
             }
-
-            webClient.DownloadFile(URL, path + "\\Документ.docx");
-
+            catch { errormessage += "- Ошибка при сортировке меток\n"; error = true; }
+            //Скачивание документа
+            try { webClient.DownloadFile(URL, patholdword); }
+            catch { errormessage += "- Ошибка при скачивании документа\n"; }
+            //Открытие документа для работы
             Document document = new Document();
-            document.LoadFromFile(path + "\\Документ.docx");
-
+            try { document.LoadFromFile(patholdword); }
+            catch { errormessage += "- Ошибка при открытии документа\n"; }
             //Текст
-            if (TextLabels.Any())
+            try
             {
-                foreach (var Data in TextLabels) document.Replace("<label>" + Data.Key + "</label>", Data.Value, false, true);
-                TextLabels.Clear();
+                if (TextLabels.Any())
+                {
+                    foreach (var Data in TextLabels) document.Replace("<label>" + Data.Key + "</label>", Data.Value, false, true);
+                    TextLabels.Clear();
+                }
             }
-
+            catch { errormessage += "- Ошибка при замене текстовых меток\n"; error = true; }
             //Изображения
-            if (PictureLabels.Any())
+            try
             {
-                foreach (var Data in PictureLabels) webClient.DownloadFile(Data.Value, path + "\\" + Data.Key + ".jpg");
-                Image[] image = new Image[100];
-                int i = 0;
-                foreach (var Data in PictureLabels)
+                if (PictureLabels.Any())
                 {
-                    image[i] = Image.FromFile(path + "\\" + Data.Key + ".jpg");
-                    i++;
-                }
+                    foreach (var Data in PictureLabels) webClient.DownloadFile(Data.Value, path + "\\" + Data.Key + ".jpg");
 
-                i = 0;
-                int index = 0;
-                DocPicture pic = new DocPicture(document);
-                TextRange range = null;
-                TextSelection[] selections = new TextSelection[100];
+                    int i = 0;
 
-                foreach (var Data in PictureLabels)
-                {
-                    pic.LoadImage(image[i]);
-                    selections[i] = document.FindString("<label>" + Data.Key + "</label>", true, true);
-                    range = selections[i].GetAsOneRange();
-                    index = range.OwnerParagraph.ChildObjects.IndexOf(range);
-                    range.OwnerParagraph.ChildObjects.Insert(index, pic);
-                    range.OwnerParagraph.ChildObjects.Remove(range);
-                    i++;
-                }
-                i = 0;
-
-                //Удаление загруженных изображений
-                //foreach (var Data in PictureLabels) System.IO.File.Delete(Path + "\\" + Data.Key + ".jpg");
-            }
-
-            //Таблицы
-            if (TableLabels.Any())
-            {
-                Table table = document.Sections[0].Tables[1] as Spire.Doc.Table;
-                TableRow row = table.Rows[1];
-                int length = 0;
-
-                foreach (var Data in TableLabels)
-                {
-                    if (Data.Value.Length > length) length = Data.Value.Length;
-                }
-
-                for (int i = 0; i < length - 1; i++)
-                {
-                    row = table.AddRow();
-                }
-
-                foreach (var Data in TableLabels)
-                {
-                    document.Replace("<label>" + Data.Key + "</label>", Data.Value[0], false, true);
-
-                    for (int s = 2, d = 1; d < Data.Value.Length; s++, d++)
+                    foreach (var Data in PictureLabels)
                     {
-                        if (Data.Key == "val1") table[s, 0].AddParagraph().AppendText(Data.Value[d]);
-                        else if (Data.Key == "val2") table[s, 1].AddParagraph().AppendText(Data.Value[d]);
-                        else if (Data.Key == "val3") table[s, 2].AddParagraph().AppendText(Data.Value[d]);
+                        using (var bmpTemp = new Bitmap(path + "\\" + Data.Key + ".jpg"))
+                            image[i] = new Bitmap(bmpTemp);
+                        i++;
+                    }
+
+                    i = 0;
+
+                    foreach (var Data in PictureLabels)
+                    {
+                        selection[i] = document.FindString("<label>" + Data.Key + "</label>", true, true);
+                        DocPicture pic = new DocPicture(document);
+                        pic.LoadImage(image[i]);
+                        range = selection[i].GetAsOneRange();
+                        index = range.OwnerParagraph.ChildObjects.IndexOf(range);
+                        range.OwnerParagraph.ChildObjects.Insert(index, pic);
+                        range.OwnerParagraph.ChildObjects.Remove(range);
+                        i++;
+                    }
+
+                    foreach (var Data in PictureLabels)
+                    {
+                        imagespaths += path + "\\" + Data.Key + ".jpg" + " ";
                     }
                 }
             }
+            catch { errormessage += "- Ошибка при замене меток изображений\n"; error = true; }
+            //Таблицы
+            try
+            {
+                if (TableLabels.Any())
+                {
+                    Table table = document.Sections[0].Tables[1] as Spire.Doc.Table;
+                    TableRow row = table.Rows[1];
+                    int length = 0;
 
-            document.SaveToFile(path + "\\Документ.docx");
+                    foreach (var Data in TableLabels)
+                    {
+                        if (Data.Value.Length > length) length = Data.Value.Length;
+                    }
 
-            return label + " " + value;
+                    for (int i = 0; i < length - 1; i++)
+                    {
+                        row = table.AddRow();
+                    }
+
+                    foreach (var Data in TableLabels)
+                    {
+                        document.Replace("<label>" + Data.Key + "</label>", Data.Value[0], false, true);
+
+                        for (int s = 2, d = 1; d < Data.Value.Length; s++, d++)
+                        {
+                            if (Data.Key == "val1") table[s, 0].AddParagraph().AppendText(Data.Value[d]);
+                            else if (Data.Key == "val2") table[s, 1].AddParagraph().AppendText(Data.Value[d]);
+                            else if (Data.Key == "val3") table[s, 2].AddParagraph().AppendText(Data.Value[d]);
+                        }
+                    }
+                }
+            }
+            catch { errormessage += "- Ошибка при замене меток таблицы\n"; error = true; }
+            //Сохранение документа
+            try { document.SaveToFile(pathnewword); }
+            catch { errormessage += "- Ошибка при сохранении документа\n"; error = true; }
+            //Время работы сервиса
+            DateTime stop = DateTime.Now;
+            var result = stop - start;
+            //Работа с БД
+            try
+            {
+                //Подготовка переменных к созданию запроса
+                patholdword = patholdword.Replace("\\", "\\\\");
+                pathnewword = pathnewword.Replace("\\", "\\\\");
+                imagespaths = imagespaths.Replace("\\", "\\\\");
+                string labelsnames = "'{ " + label + " }'";
+                string labelsvalues = "'{ " + value + " }'";
+                //Добавление записи в БД
+                string command1 = $@"INSERT INTO public.""ServiceResults""
+                (""FileOld"", ""FileNew"", ""LabelsNames"", ""LabelsValues"", ""WorkingTime"")
+                VALUES (bytea('{patholdword}'), bytea('{pathnewword}'), {labelsnames}, {labelsvalues}, interval '{result}');";
+
+                string command2 = $@"INSERT INTO public.""ServiceResults""
+                (""FileOld"", ""FileNew"", ""LabelsNames"", ""LabelsValues"", ""Images"", ""WorkingTime"")
+                VALUES (bytea('{patholdword}'), bytea('{pathnewword}'), {labelsnames}, {labelsvalues}, bytea('{imagespaths}'), interval '{result}');";
+                NpgsqlCommand com;
+                if (imagespaths == "") com = new NpgsqlCommand(command1, conn);
+                else com = new NpgsqlCommand(command2, conn);
+                conn.Open();
+                com.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch { errormessage += "- Ошибка при отправке запроса в таблицу базы данных\n"; error = true; }
+            //Удаление документа и изображений
+            try { System.IO.File.Delete(patholdword); }
+            catch { errormessage += "- Ошибка при удалении документа\n"; error = true; }
+            try { foreach (var Data in PictureLabels) System.IO.File.Delete(path + "\\" + Data.Key + ".jpg"); }
+            catch { errormessage += "- Ошибка при удалении изображений\n"; error = true; }
+            //Очищение словарей
+            TextLabels.Clear();
+            PictureLabels.Clear();
+            TableLabels.Clear();
+            //Подготовка сообщения
+            if (error == true)
+            {
+                ViewBag.Title = "Error";
+                ViewBag.Message = errormessage;
+            }
+            else
+            {
+                ViewBag.Title = "Not Error";
+                ViewBag.Message = "Документ был успешно создан. Полный путь к документу: " + pathnewword + "\nВремя, затраченное на создание документа: " + result.ToString("hh':'mm':'ss");
+            }
+
+            return View();
         }
     }
 }
